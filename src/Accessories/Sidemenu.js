2721330570d297ef4f48320b09e564468000003e
CrazyTolstoy <mirkovicvojislav20@gmail.com>
2023-04-21 08:10:46 +0200

    Commit

--- src/Accessories/Sidemenu.js
+++ src/Accessories/Sidemenu.js
@@ -19,6 +19,8 @@ export default function Sidemenu(){
                 </li>
                 <li>
                     <Link to="/api-page" className="menu__item">API</Link>
+                </li> <li>
+                    <Link to="/api-page" className="menu__item">API</Link>
                 </li>
             </ul>
         </div>
