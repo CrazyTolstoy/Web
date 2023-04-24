f9be68ce6dbf1dc490cf5daaf26744de58252aa7
CrazyTolstoy <mirkovicvojislav20@gmail.com>
2023-04-24 21:45:12 +0200

    contextMenu

--- src/Accessories/Grafik.js
+++ src/Accessories/Grafik.js
@@ -1,6 +1,45 @@
 import Highcharts from 'highcharts';
+import HighchartsExporting from 'highcharts/modules/exporting';
 
-export const createChart = (xvalue,yValue1, yValue2, yValue3) => {
+export const createChart = (xvalue, yValue1, yValue2, yValue3) => {
+  HighchartsExporting(Highcharts);
+
+  const contextMenu = {
+    items: {
+      downloadPNG: {
+        text: 'Download PNG',
+        onclick: function () {
+          this.exportChart();
+        }
+      },
+      downloadJPEG: {
+        text: 'Download JPEG',
+        onclick: function () {
+          this.exportChart({
+            type: 'image/jpeg'
+          });
+        }
+      },
+      downloadPDF: {
+        text: 'Download PDF',
+        onclick: function () {
+          this.exportChart({
+            type: 'application/pdf'
+          });
+        }
+      },
+      downloadSVG: {
+        text: 'Download SVG',
+        onclick: function () {
+          this.exportChart({
+            type: 'image/svg+xml'
+          });
+        }
+      }
+    }
+  }
+
+
   const options = { 
     chart: {
       type: 'line'
